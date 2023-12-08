92f84200a47663932f1e903ad60eba20f5b2cb84
Marcos <marcosraiquenmolina@gmail.com>
2023-12-08 00:29:45 -0300

    Interfaz Crud Terminada

--- src/components/ShowProducts.jsx
+++ src/components/ShowProducts.jsx
@@ -1,7 +1,6 @@
-import React,{Component, useEffect, useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import axios from "axios"
 import Swal from "sweetalert2"
-import withReactContent from "sweetalert2-react-content"
 import { show_alerta } from '../functions'
 
 const ShowProducts = () => {
